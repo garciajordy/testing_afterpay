1b75b9f501b13d3fe6e77001c1cee9fe25024e81
garciajordy <jordygarcia0119@gmail.com>
2021-08-30 14:34:40 +0300

    test solidus afterpay

---
+++ db/migrate/20210830112559_create_solidus_afterpay_payment_sources.solidus_afterpay.rb
@@ -0,0 +1,13 @@
+# This migration comes from solidus_afterpay (originally 20210813142725)
+class CreateSolidusAfterpayPaymentSources < SolidusSupport::Migration[4.2]
+  def change
+    create_table :solidus_afterpay_payment_sources do |t|
+      t.string :token
+      t.references :payment_method, index: true
+
+      t.timestamps
+    end
+
+    add_foreign_key :solidus_afterpay_payment_sources, :spree_payment_methods, column: :payment_method_id
+  end
+end
